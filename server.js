0ccf4c2b71131d87d49c35892ed585c7bfd2d234
Leslie Shea <leslieyshea@gmail.com>
2019-04-16 15:39:48 -0700

    post test passing

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 const app = require('./lib/app');
 
+const PORT = process.env.PORT || 7890;
+
 mongoose.connect('mongodb://localhost:27017/dogs', {
-  useFindAndModify: false,
   useNewUrlParser: true,
+  useFindAndModify: false,
   useCreateIndex: true
 });
 
-app.listen(7890, () => {
+app.listen(PORT, () => {
   /*eslint-disable-next-line*/
-  console.log('Started on port 7890');
+  console.log(`Started on port ${PORT}`);
 });
--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../lib/app');
+// const Dog = require('../lib/models/Dog');
 
 describe('tweet routes', () => {
   beforeAll(() => {
-    return mongoose.connect('mongodb://localhose:27107/dog', {
-      useFindAndModify: false,
+    return mongoose.connect('mongodb://localhost:27017/dogs', {
       useNewUrlParser: true,
+      useFindAndModify: false,
       useCreateIndex: true
     });
   });
@@ -17,5 +18,24 @@ describe('tweet routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('can create a new dog', () => {
+    return request(app)
+      .post('/dogs')
+      .send({
+        name: 'buddy',
+        age: 2,
+        breed: 'mini goldendoodle'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'buddy',
+          age: 2,
+          breed: 'mini goldendoodle',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
   });
 });
