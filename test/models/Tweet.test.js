73ff55f46ebfc68cbb76d64f0727b6babfe603ec
Leslie Shea <leslieyshea@gmail.com>
2019-04-16 14:02:48 -0700

    tests passing

--- test/models/Tweet.test.js
+++ test/models/Tweet.test.js
@@ -14,4 +14,36 @@ describe('Tweet model', () => {
       _id: expect.any(mongoose.Types.ObjectId)
     });
   });
+
+  it('has a required handle field', () => {
+    const tweet = new Tweet({
+      body: 'my second tweet'
+    });
+
+    const errors = tweet.validateSync().errors;
+
+    expect(errors.handle.message).toEqual('Path `handle` is required.');
+  });
+
+  it('has a required body field', () => {
+    const tweet = new Tweet({
+      handle: 'leslie'
+    });
+
+    const errors = tweet.validateSync().errors;
+
+    expect(errors.body.message).toEqual('Path `body` is required.');
+  });
+
+  it('has a body with maxlength of 256', () => {
+    const body = 'a'.repeat(300);
+    const tweet = new Tweet({
+      handle: 'leslie',
+      body
+    });
+
+    const errors = tweet.validateSync().errors;
+
+    expect(errors.body.message).toEqual(`Path \`body\` (\`${body}\`) is longer than the maximum allowed length (256).`);
+  });
 });
