3ea7ebcdb8c042dc1e25749851c431578db1be36
Leslie Shea <leslieyshea@gmail.com>
2019-04-16 11:43:27 -0700

    created a tweet

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -1,5 +1,25 @@
-
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/tweets', {
   useNewUrlParser: true
 });
+
+const tweetSchema = new mongoose.Schema({
+  handle: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+Tweet
+  .create({
+    handle: 'leslie',
+    body: 'my first tweet'
+  })
+  .then(createdTweet => console.log(createdTweet));
