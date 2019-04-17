ef71367f51759bd515c57a46cb2c174577b480f1
Leslie Shea <leslieyshea@gmail.com>
2019-04-17 09:22:52 -0700

    added error handling

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ mongoose.connect('mongodb://localhost:27017/dogs', {
   useCreateIndex: true
 });
 
+/*eslint-disable-next-line no-unused-vars*/
+app.use((err, req, res, next) => {
+  req.statusCode(500).send({ error: err });
+});
+
 app.listen(PORT, () => {
   /*eslint-disable-next-line*/
   console.log(`Started on port ${PORT}`);
